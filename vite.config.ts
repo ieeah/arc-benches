9b39ff16b589dc0879999a6d5d84832c9f115db1
ieeah <lbnfnc90@gmail.com>
2026-06-19 01:34:43 +0200

    fix: try fix pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   // Served from https://ieeah.github.io/arc-benches/
   base: "/arc-benches/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
